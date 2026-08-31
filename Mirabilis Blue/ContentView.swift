653fa633653ae08c35e105c769549971b86ebd73
UedaSoft IT Solutions <max.ueda@uedasoft-it.com>
2026-08-31 02:33:23 -0300

    Initial Commit

---
+++ Mirabilis Blue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mirabilis Blue
+//
+//  Created by Max Ueda on 31/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mirabilis Blue/Mirabilis_BlueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mirabilis_BlueApp.swift
+//  Mirabilis Blue
+//
+//  Created by Max Ueda on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Mirabilis_BlueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
